8d03583ddfda51b933340161002b3d4be774c074
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-03-13 16:11:38 +0100

    Logo animation, navbar links:hover, overlay menu, hero logo

--- js/index.js
+++ js/index.js
@@ -1,31 +1,48 @@
 // QUERY SELECTORS =================================================
 const navbar = document.querySelector(".navbar");
+const navbarLinks = document.querySelectorAll(".navbar-link");
 const menuIcon = document.querySelector(".menu-icon");
+const navbarLogo = document.querySelector(".navbar-logo");
 const closeBtn = document.querySelector(".close-btn");
 const overlay = document.querySelector(".overlay");
 
 
 // EVENT LISTENER FUNCTIONS ========================================
 
+
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 const scrollFunction = () => {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    navbar.style.padding = ".5rem 0rem";
-    navbar.style.backgroundColor = "rgba(255, 255, 255, .98)";
-    // document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    navbar.style.padding = "1.5rem 0rem";
-    navbar.style.backgroundColor = "rgba(0, 0, 0, .1)";
-    // document.getElementById("logo").style.fontSize = "35px";
+    navbar.style.padding = ".5rem 1.5rem";
+    navbar.style.backgroundColor = "rgba(255, 255, 255, .95)";
+    navbarLogo.src = "./images/cubelogodark.png"
+    // sneaky go-around to fool javascript (changing the font-color and the :before pseudo class background-color)
+    toggleColor("rgba(0, 0, 0, 1)", "dark")
+  }
+  else {
+    navbar.style.padding = "1.5rem";
+    navbar.style.backgroundColor = "transparent";
+    navbarLogo.src = "./images/cubelogo.png"
+    // sneaky go-around to fool javascript (changing the font-color and the :before pseudo class background-color)
+    toggleColor("rgba(255, 255, 255, 1)", "light")
   }
 };
 
+const toggleColor = (color, action) => {
+  navbarLinks.forEach(link => {
+    link.style.color = `${color}`;
+    action === "dark" ?
+      link.className = "navbar-link change-background" :
+      link.className = "navbar-link";
+  });
+};
+
 const openNav = () => {
-  overlay.style.height = "100%";
+  overlay.style.width = "100%";
 };
 
 const closeNav = () => {
-  overlay.style.height = "0%";
+  overlay.style.width = "0%";
 };
 
 
