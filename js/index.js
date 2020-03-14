fcfc8361b5b10e2f36c8ee12dd3f19779b1b646e
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-03-14 14:47:13 +0100

    added fav icon and styled the scroll bar

--- js/index.js
+++ js/index.js
@@ -6,13 +6,10 @@ const navbarLogo = document.querySelector(".navbar-logo");
 const closeBtn = document.querySelector(".close-btn");
 const overlay = document.querySelector(".overlay");
 const overlayLinks = document.querySelectorAll(".overlay-link");
-
 const welcomeMsg = document.querySelector(".welcome-msg");
 
 
 // EVENT LISTENER FUNCTIONS ========================================
-
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 const toggleColor = (color, action) => {
   navbarLinks.forEach(link => {
     link.style.color = `${color}`;
@@ -22,7 +19,7 @@ const toggleColor = (color, action) => {
   });
 };
 
-
+// When the user scrolls down 80px from the top of the document, run some animations
 const scrollFunction = () => {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     navbar.style.padding = ".5rem 1.5rem";
@@ -36,7 +33,6 @@ const scrollFunction = () => {
     navbar.style.padding = "1.5rem";
     navbar.style.backgroundColor = "transparent";
     navbarLogo.src = "./images/cubelogo.png";
-    // welcomeMsg.classList.remove("is-visible");
     // sneaky go-around to fool javascript (changing the font-color and the :before pseudo class background-color)
     toggleColor("rgba(255, 255, 255, 1)", "light");
   }
