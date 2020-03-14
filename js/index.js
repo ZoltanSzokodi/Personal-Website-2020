9e04b44828f3b0b0a4b52304b9b70d33d398d5c3
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-03-14 13:30:58 +0100

    Refactored the hero alingment and added welcome text with animation

--- js/index.js
+++ js/index.js
@@ -7,28 +7,12 @@ const closeBtn = document.querySelector(".close-btn");
 const overlay = document.querySelector(".overlay");
 const overlayLinks = document.querySelectorAll(".overlay-link");
 
+const welcomeMsg = document.querySelector(".welcome-msg");
+
 
 // EVENT LISTENER FUNCTIONS ========================================
 
-
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-const scrollFunction = () => {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    navbar.style.padding = ".5rem 1.5rem";
-    navbar.style.backgroundColor = "rgba(255, 255, 255, .95)";
-    navbarLogo.src = "./images/cubelogodark.png"
-    // sneaky go-around to fool javascript (changing the font-color and the :before pseudo class background-color)
-    toggleColor("rgba(0, 0, 0, 1)", "dark")
-  }
-  else {
-    navbar.style.padding = "1.5rem";
-    navbar.style.backgroundColor = "transparent";
-    navbarLogo.src = "./images/cubelogo.png"
-    // sneaky go-around to fool javascript (changing the font-color and the :before pseudo class background-color)
-    toggleColor("rgba(255, 255, 255, 1)", "light")
-  }
-};
-
 const toggleColor = (color, action) => {
   navbarLinks.forEach(link => {
     link.style.color = `${color}`;
@@ -36,6 +20,26 @@ const toggleColor = (color, action) => {
       link.className = "navbar-link change-background" :
       link.className = "navbar-link";
   });
+};
+
+
+const scrollFunction = () => {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    navbar.style.padding = ".5rem 1.5rem";
+    navbar.style.backgroundColor = "rgba(255, 255, 255, .95)";
+    navbarLogo.src = "./images/cubelogodark.png"
+    welcomeMsg.classList.add("is-visible")
+    // sneaky go-around to fool javascript (changing the font-color and the :before pseudo class background-color)
+    toggleColor("rgba(0, 0, 0, 1)", "dark");
+  }
+  else {
+    navbar.style.padding = "1.5rem";
+    navbar.style.backgroundColor = "transparent";
+    navbarLogo.src = "./images/cubelogo.png";
+    // welcomeMsg.classList.remove("is-visible");
+    // sneaky go-around to fool javascript (changing the font-color and the :before pseudo class background-color)
+    toggleColor("rgba(255, 255, 255, 1)", "light");
+  }
 };
 
 const openNav = () => {
@@ -55,4 +59,14 @@ overlayLinks.forEach(link => link.addEventListener("click", closeNav));
 
 
 
+// function myFunction(x) {
+//   if (x.matches) { // If media query matches
+//     heroImg.src = "./images/banner-standing.jpg";
+//   } else {
+//     heroImg.src = "./images/abstract-banner.jpg";
+//   }
+// }
 
+// var x = window.matchMedia("(max-width: 900px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
